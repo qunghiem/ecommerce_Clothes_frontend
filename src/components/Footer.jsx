357ea7d7b6b52addc99b6c40ae8ca5c425fe476e
Quân Nghiêm <nghiemxuanquan2003@gmail.com>
2025-08-06 11:52:42 +0700

    update logo icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div>
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm ">
         <div>
-          <img src={assets.logo} className="mb-5 w-32 " alt="" />
+          <img src={assets.adamoLogo} className="mb-5 w-32 " alt="" />
           <p className="w-full sm:w-2/3 text-gray-600">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
